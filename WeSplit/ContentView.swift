e7736c1301d1f54130b9173bcae29ca13c62c7bb
Lester Batres <swat1@mnc.gt>
2020-01-21 20:12:21 -0600

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Lester Batres on 1/21/20.
+//  Copyright © 2020 lester. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
